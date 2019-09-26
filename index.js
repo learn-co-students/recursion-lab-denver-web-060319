f16660665f0c44adc03cc35f461cc11082798c3c
foggybrains <dustin.keyes@gmail.com>
2019-09-25 18:37:08 -0600

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 // Code your solution here!
 
+const string = 'hey buddy!'
+
+function reverseString(string) {
+    console.log(string[-1])
+    if (string.length > 1) {
+        string.pop()
+        reverseString(string)
+    } else {
+        return true
+    }
+}
